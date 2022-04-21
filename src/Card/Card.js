91b6be86babbeb8fd6329dd4203b0d6acd9ab4e7
dang-dev <aljohnvillacruel141@gmail.com>
2022-04-21 11:08:22 +0800

    initial commit

---
+++ src/Card/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = (props) => {
+    const {title, name, age, gender} = props
+
+    return (
+        <div className="card-main">
+            <h1>Users: Card</h1>
+            <div>Name: { name}</div>
+            <div>Age: { age}</div>
+            <div>Gender: { gender }</div>
+        </div>
+    )
+}
+
+export default Card
